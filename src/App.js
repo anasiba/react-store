2667561100402624315362f08d2133847c935e27
anasiba <anasibarki1997@gmail.com>
2019-08-04 13:18:35 +0100

    add a router systeme to menu-item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
- import "./App.css";
- import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
-  return <div>
-    <HomePage/>
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
- 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
- 
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
